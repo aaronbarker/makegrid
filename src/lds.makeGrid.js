b9bffe147fa7de54cddc8accc9b5f40f063a7184
Aaron Barker <github@zbarkers.com>
2013-11-21 16:43:26 -0700

    adding some warnings for things that are required adding selectorExclude. sometimes there are elements you don't want included (cms added things, positioned elements, etc) and this allows them to be excluded changing logic for adding first/last class. The nth-child wouldn't work with the exclude stuff above, so moved to a modulus solution

--- src/lds.makeGrid.js
+++ src/lds.makeGrid.js
@@ -1,17 +1,18 @@
 /*!
- * makeGrid
- * @description	Dynamically creates rows/cols for grids of images/blocks responsively
- * @version		@@version - @@date
- * @author		Aaron Barker
- * @requires	ui.widget.js
- * @copyright	Copyright Intellectual Reserve Inc. All rights reserved.
- */
+* makeGrid
+* @description	Dynamically creates rows/cols for grids of images/blocks responsively
+* @version		@@version - @@date
+* @author		Aaron Barker
+* @requires	ui.widget.js
+* @copyright	Copyright Intellectual Reserve Inc. All rights reserved.
+*/
 (function($) {
 	"use strict";
 	$.widget("lds.makeGrid", {
 		options: {
 			firstClass:"first",
 			lastClass:"last",
+			selectorExclude:false, // sometimes there are elements you don't want included (cms added things, positioned elements, etc)
 			setupResize:function(event,self){ // declared as an option so other resize solutions can be used, such as a throttle
 				$(window).on("resize orientationchange",function(){
 					self.fit();
@@ -31,7 +32,7 @@
 		fit:function(){
 			// console.debug("method: fit");
 			var opts = this.options,  elem = this.element,
-				kids = elem.children(),
+				kids = elem.children().not(opts.selectorExclude),
 				firstClass = opts.firstClass,
 				lastClass = opts.lastClass,
 				maxCols = elem.data("cols")||opts.maxCols,
@@ -43,6 +44,17 @@
 				wrapperRightX = elem.offset().left + elem.width(),
 				totalMargin,elemRemainder,colWidthPX,colWidth,lastOfRow;
 
+			// make sure a few required things are available
+			if(firstKid.css("float") === "none"){
+				// if the kids aren't floated things go bad. So make sure they are
+				console.debug("MakeGrid: kids aren't floated");
+				return;
+			}
+			if(!firstKid.outerHeight(true)){
+				// if the kids don't have a height (due to absolutely positioning sub-elements, or images not loaded yet) things go bad. So make sure we have some kind of height
+				console.debug("MakeGrid: kids don't have height");
+				return;
+			}
 			kids.removeClass(firstClass+" "+lastClass);
 
 			if(numCols > maxCols) {
@@ -63,8 +75,15 @@
 
 			// run any logic for making rows fit
 			if(numCols > 1){
-				kids.filter(":nth-child("+numCols+"n)").addClass(lastClass);
-				kids.filter(":nth-child("+numCols+"n-"+(numCols-1)+")").addClass(firstClass);
+				// nth-child wouldn't work when the selectorExclude was being used
+				kids.each(function(index){
+					if(index%numCols === 0){
+						$(this).addClass(firstClass);
+					}
+					if(index%numCols === numCols-1){
+						$(this).addClass(lastClass);
+					}
+				});
 			}
 
 			// occasionally with a decimal margin, things don't add up. Make sure they do
