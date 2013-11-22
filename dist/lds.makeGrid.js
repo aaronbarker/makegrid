6a879f3d738fe776313bc7f8e8a35ead0ad236af
Aaron Barker <github@zbarkers.com>
2013-11-21 17:05:02 -0700

    doing my best to screw up the git history. no clue what I am doing.
    This is not a final 1.2.0 yet, but wanted to get everything checked in before I lost stuff

--- dist/lds.makeGrid.js
+++ dist/lds.makeGrid.js
@@ -1,17 +1,18 @@
 /*!
- * makeGrid
- * @description	Dynamically creates rows/cols for grids of images/blocks responsively
- * @version		1.1.3 - 2013/10/08
- * @author		Aaron Barker
- * @requires	ui.widget.js
- * @copyright	Copyright Intellectual Reserve Inc. All rights reserved.
- */
+* makeGrid
+* @description	Dynamically creates rows/cols for grids of images/blocks responsively
+* @version		1.2.0 - 2013/11/21
+* @author		Aaron Barker
+* @requires	ui.widget.js
+* @copyright	Copyright Intellectual Reserve Inc. All rights reserved.
+*/
 (function($) {
 	"use strict";
 	$.widget("lds.makeGrid", {
 		options: {
 			firstClass:"first",
 			lastClass:"last",
+			selectorExclude:false, // sometimes there are elements you don't want included (cms added things, positioned elements, etc)
 			setupResize:function(event,self){ // declared as an option so other resize solutions can be used, such as a throttle
 				$(window).on("resize orientationchange",function(){
 					self.fit();
@@ -31,7 +32,7 @@
 		fit:function(){
 			// console.debug("method: fit");
 			var opts = this.options,  elem = this.element,
-				kids = elem.children(),
+				kids = elem.children().not(opts.selectorExclude),
 				firstClass = opts.firstClass,
 				lastClass = opts.lastClass,
 				maxCols = elem.data("cols")||opts.maxCols,
@@ -43,6 +44,17 @@
 				wrapperRightX = elem.offset().left + elem.width(),
 				totalMargin,elemRemainder,colWidthPX,colWidth,lastOfRow;
 
+			// make sure a few required things are available
+			if(firstKid.css("float") === "none"){
+				// if the kids aren't floated things go bad. So make sure they are
+				console.debug("MakeGrid: kids aren't floated");
+				return;
+			}
+			if(!firstKid.outerHeight(true)){
+				// if the kids don't have a height (due to absolutely positioning sub-elements, or images not loaded yet) things go bad. So make sure we have some kind of height
+				console.debug("MakeGrid: kids don't have height");
+				return;
+			}
 			kids.removeClass(firstClass+" "+lastClass);
 
 			if(numCols > maxCols) {
@@ -63,14 +75,22 @@
 
 			// run any logic for making rows fit
 			if(numCols > 1){
-				kids.filter(":nth-child("+numCols+"n)").addClass(lastClass);
-				kids.filter(":nth-child("+numCols+"n-"+(numCols-1)+")").addClass(firstClass);
+				// nth-child wouldn't work when the selectorExclude was being used
+				kids.each(function(index){
+					if(index%numCols === 0){
+						$(this).addClass(firstClass);
+					}
+					if(index%numCols === numCols-1){
+						$(this).addClass(lastClass);
+					}
+				});
 			}
 
 			// occasionally with a decimal margin, things don't add up. Make sure they do
 			lastOfRow = kids.eq(numCols-1);
 			// console.debug(lastOfRow,wrapperRightX,lastOfRow.offset().left + lastOfRow.outerWidth(true));
 			if(!opts.isIE8){ // ie8 siezes up on this loop, so don't do it in ie8
+				var x = 0;
 				while(lastOfRow.length && (wrapperRightX - (lastOfRow.offset().left + lastOfRow.outerWidth(true)) > 10)){
 					/*  Nice round numbers like 0.1 cause interesting floating
 						point rounding errors because they cannot be accuratly 
@@ -82,11 +102,16 @@
 
 					// last column is the same as first colum, there is a width issue
 					kids.css("width",colWidth+"%");
+					// in rare circumstances this goes into an endless loop. the following is a safety valve
+					if(x > 10){
+						return;
+					}
+					x++;
 				}
 			}
 		}
 	});
 	$.extend($.lds.makeGrid, {
-		version: "1.1.3"
+		version: "1.2.0"
 	});
 })(jQuery);
